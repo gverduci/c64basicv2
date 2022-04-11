98c1922b9e07492af0ae1ddf29b4a1d1e666762f
gverduci <g.verduci@gmail.com>
2022-04-11 21:29:31 +0200

    check configuration

--- src/commands/convert.ts
+++ src/commands/convert.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { spawnSync } from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as TokenizedBASICFile from "./tokenizedBASICFile";
 
 export function convert () : TokenizedBASICFile.TokenizedBASICFile {
@@ -11,68 +12,79 @@ export function convert () : TokenizedBASICFile.TokenizedBASICFile {
 		const showDiagnostics : boolean | undefined = configuration.get("showDiagnostics");
 		const outputRelativeDir : string | undefined = configuration.get("convertOutputDir");
 		let command : string | undefined = configuration.get("petcat");
-		const rootFolder = vscode.workspace.workspaceFolders ? path.normalize(vscode.workspace.workspaceFolders[0].uri.path) : undefined;
-		const fileBasename = path.basename(document.fileName);
-		const outputDir = `.${path.sep}${outputRelativeDir}${path.sep}`;
-		const outputFile = `${rootFolder}${path.sep}${outputRelativeDir}${path.sep}${fileBasename}`;
-
-		const basePetcatOptions = [
-			"-w2", 
-			"-o",
-			outputFile,
-			"--",
-			document.fileName
-		];
-		let petcatOptions = [];
-		if (process.platform === "win32"){
-			petcatOptions = ['/c', `"${command}"`];
-				basePetcatOptions.forEach(o=>{
-					let arg = o;
-					if (arg.indexOf("\\c:\\") > -1){
-						arg=arg.replace("\\c:\\", "c:\\");
-					}
-					if (arg.indexOf(" ") > -1){
-						arg = `"${arg}`;
-					}
-					petcatOptions.push(arg);
-				});
-			command = process.env.ComSpec || "cmd.exe";
-		} else{
-			petcatOptions = basePetcatOptions;
-		}		
-	
-		if (showDiagnostics) {
-			const output = vscode.window.createOutputChannel('c64basicv2 convert');
-            output.show(true);
-            output.appendLine("");
-            output.appendLine("**** COMMODORE 64 BASIC V2 ****");
-            output.appendLine("c64basicv2 diagnostics");
-            output.appendLine("");
-            output.appendLine(`Platform       : ${process.platform}`);
-			output.appendLine(`Current Dir    : ${rootFolder}`);
-            output.appendLine(`Current File   : ${document.fileName}`);
-            output.appendLine(`Command        : ${command}`);
-            output.appendLine(`Command Options:`);
-            output.appendLine("");
-            for (var i = 0; i < petcatOptions.length; i++) {
-                output.appendLine(`  ${petcatOptions[i]}`);
-            }
-            output.appendLine("");
-            output.appendLine("");
-		}
-
-		if (command){
-			const petcat = spawnSync(command, petcatOptions, {shell: true});
-			if (petcat){
-				let errorCode = petcat.status;
-				if (errorCode && errorCode > 0) {
-					vscode.window.showErrorMessage(`${fileBasename} conversion error!`);
+		if (command && fs.existsSync(command)) {
+			const rootFolder = vscode.workspace.workspaceFolders ? path.normalize(vscode.workspace.workspaceFolders[0].uri.path) : undefined;
+			const fileBasename = path.basename(document.fileName);
+			const outputDir = `${rootFolder}${path.sep}${outputRelativeDir}`;
+			const outputFile = `${outputDir}${path.sep}${fileBasename}`;
+			if (outputDir && fs.existsSync(outputDir)) {
+				const basePetcatOptions = [
+					"-w2", 
+					"-o",
+					outputFile,
+					"--",
+					document.fileName
+				];
+				let petcatOptions = [];
+				if (process.platform === "win32"){
+					petcatOptions = ['/c', `"${command}"`];
+						basePetcatOptions.forEach(o=>{
+							let arg = o;
+							if (arg.indexOf("\\c:\\") > -1){
+								arg=arg.replace("\\c:\\", "c:\\");
+							}
+							if (arg.indexOf(" ") > -1){
+								arg = `"${arg}`;
+							}
+							petcatOptions.push(arg);
+						});
+					command = process.env.ComSpec || "cmd.exe";
 				} else{
-					vscode.window.showInformationMessage(`${fileBasename} converted!`);
+					petcatOptions = basePetcatOptions;
+				}		
+		
+				if (showDiagnostics) {
+					const output = vscode.window.createOutputChannel('c64basicv2 convert');
+					output.show(true);
+					output.appendLine("");
+					output.appendLine("**** COMMODORE 64 BASIC V2 ****");
+					output.appendLine("c64basicv2 diagnostics");
+					output.appendLine("");
+					output.appendLine(`Platform       : ${process.platform}`);
+					output.appendLine(`Current Dir    : ${rootFolder}`);
+					output.appendLine(`Current File   : ${document.fileName}`);
+					output.appendLine(`Command        : ${command}`);
+					output.appendLine(`Command Options:`);
+					output.appendLine("");
+					for (var i = 0; i < petcatOptions.length; i++) {
+						output.appendLine(`  ${petcatOptions[i]}`);
+					}
+					output.appendLine("");
+					output.appendLine("");
+				}
+
+				if (command){
+					const petcat = spawnSync(command, petcatOptions, {shell: true});
+					if (petcat){
+						let errorCode = petcat.status;
+						if (errorCode && errorCode > 0) {
+							vscode.window.showErrorMessage(`${fileBasename} conversion error!`);
+						} else{
+							vscode.window.showInformationMessage(`${fileBasename} converted!`);
+						}
+					}
+					return {outputFile, outputDir};
+				}
+			}else{
+				if (outputDir){
+					vscode.window.showErrorMessage(`c64basicv2: "${outputDir}" folder doesn't exist!`);
+				}else{
+					vscode.window.showErrorMessage(`c64basicv2.convertOutputDir: output folder error!`);
 				}
 			}
-			return {outputFile, outputDir};
-	    }
+		}else{
+			vscode.window.showErrorMessage(`c64basicv2.petcat: configuration error!`);
+		}
 	}
 	return {outputFile:"", outputDir:""};
 };
--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,64 +1,67 @@
 import * as vscode from 'vscode';
 import { spawn } from 'child_process';
-import * as path from 'path';
+import * as fs from 'fs';
 import * as TokenizedBASICFile from "./tokenizedBASICFile";
 export function run (tokenizedBASICFile: TokenizedBASICFile.TokenizedBASICFile) {
 	if (tokenizedBASICFile.outputFile){
 		const configuration = vscode.workspace.getConfiguration('c64basicv2');
 		const showDiagnostics : boolean | undefined = configuration.get("showDiagnostics");
 		let command : string | undefined = configuration.get("x64sc");
+		if (command && fs.existsSync(command)) {
+			const rootFolder = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.path : undefined;
+			let baseX64scOptions = [
+				tokenizedBASICFile.outputFile
+			];
 
-		const rootFolder = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.path : undefined;
-		let baseX64scOptions = [
-			tokenizedBASICFile.outputFile
-		];
-
-		let x64scOptions = [];
-		if (process.platform === "win32"){
-			x64scOptions = ['/c', `"${command}"`];
-			baseX64scOptions.forEach(o=>{
-				let arg = o;
-				if (arg.indexOf("\\c:\\") > -1){
-					arg=arg.replace("\\c:\\", "c:\\");
-				}
-				if (arg.indexOf(" ") > -1){
-					arg = `"${arg}`;
-				}
-				x64scOptions.push(arg);
-			});
-			command = process.env.ComSpec || "cmd.exe";
-		} else{
-			x64scOptions = baseX64scOptions;
-		}
-
-		if (showDiagnostics) {
-			const output = vscode.window.createOutputChannel('c64basicv2 Run');
-            output.show(true);
-            output.appendLine("");
-            output.appendLine("**** COMMODORE 64 BASIC V2 ****");
-            output.appendLine("c64basicv2 diagnostics");
-            output.appendLine("");
-            output.appendLine(`Platform       : ${process.platform}`);
-            output.appendLine(`Current Dir    : ${rootFolder}`);
-			output.appendLine(`Current File   : ${tokenizedBASICFile.outputFile}`);
-            output.appendLine(`Command        : ${command}`);
-            output.appendLine(`Command Options:`);
-            output.appendLine("");
-            for (var i = 0; i < x64scOptions.length; i++) {
-                output.appendLine(`  ${x64scOptions[i]}`);
-            }
-            output.appendLine("");
-            output.appendLine("");
-		}
-		if (command) {
-			let x64sc = spawn(command, x64scOptions,
-				{
-					detached: true,
-					stdio: 'inherit',
-					shell: true
+			let x64scOptions = [];
+			if (process.platform === "win32"){
+				x64scOptions = ['/c', `"${command}"`];
+				baseX64scOptions.forEach(o=>{
+					let arg = o;
+					if (arg.indexOf("\\c:\\") > -1){
+						arg=arg.replace("\\c:\\", "c:\\");
+					}
+					if (arg.indexOf(" ") > -1){
+						arg = `"${arg}`;
+					}
+					x64scOptions.push(arg);
 				});
-			x64sc.unref();
+				command = process.env.ComSpec || "cmd.exe";
+			} else{
+				x64scOptions = baseX64scOptions;
+			}
+
+			if (showDiagnostics) {
+				const output = vscode.window.createOutputChannel('c64basicv2 Run');
+				output.show(true);
+				output.appendLine("");
+				output.appendLine("**** COMMODORE 64 BASIC V2 ****");
+				output.appendLine("c64basicv2 diagnostics");
+				output.appendLine("");
+				output.appendLine(`Platform       : ${process.platform}`);
+				output.appendLine(`Current Dir    : ${rootFolder}`);
+				output.appendLine(`Current File   : ${tokenizedBASICFile.outputFile}`);
+				output.appendLine(`Command        : ${command}`);
+				output.appendLine(`Command Options:`);
+				output.appendLine("");
+				for (var i = 0; i < x64scOptions.length; i++) {
+					output.appendLine(`  ${x64scOptions[i]}`);
+				}
+				output.appendLine("");
+				output.appendLine("");
+			}
+			if (command) {
+				let x64sc = spawn(command, x64scOptions,
+					{
+						detached: true,
+						stdio: 'inherit',
+						shell: true
+					});
+				x64sc.unref();
+			}
+		}else{
+			vscode.window.showErrorMessage(`c64basicv2.x64sc: configuration error!`);
 		}
-        return;
 	}
+	return;
 };

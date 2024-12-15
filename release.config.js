41f24cce822e5f05daf91ac4633357b7b74f6406
gverduci <g.verduci@gmail.com>
2024-12-15 10:55:26 +0100

    test: test semantic-release

---
+++ release.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  branches: ["develop"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogTitle: "# Release notes",
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
+			'@semantic-release/npm',
+			{
+				npmPublish: false,
+			},
+		],
+    [
+        "semantic-release-vsce",
+        {
+          packageVsix: true,
+          publish: false
+        }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "./*.vsix" },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md", "package.json"],
+      },
+    ],
+  ],
+  preset: "conventionalcommits",
+};
